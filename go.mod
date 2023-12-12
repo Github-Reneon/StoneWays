6b4f0a42ee7d689efae780d195b4503b3be51875
Github-Reneon <andrewlpriv@gmail.com>
2023-12-12 22:38:35 +0000

    updated dice rolling package and tried it

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module StoneWays
 
 go 1.21.3
 
-require github.com/Github-Reneon/dicetools v0.0.0-20231212150349-54a102f465d9
+require github.com/Github-Reneon/dicetools v0.0.0-20231212221041-5a61a9e99347
--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
-import (
-	"fmt"
-
-	dicetools "github.com/Github-Reneon/dicetools"
-)
+import "github.com/Github-Reneon/dicetools"
 
 func main() {
-	fmt.Println(dicetools.RollNotation("2d2"))
+	player := new(PC)
+
+	//base stats
+	player.Max_STR = dicetools.RollNotation("3d6")
+	player.Max_DEX = dicetools.RollNotation("3d6")
+	player.Max_Will = dicetools.RollNotation("3d6")
+	player.STR = player.Max_STR
+	player.DEX = player.Max_DEX
+	player.Will = player.Max_Will
+
+	//health
+	player.Max_HP = dicetools.RollNotation("1d6")
+	player.HP = player.Max_HP
+
+	//gold
+	player.Gold = dicetools.RollNotation("3d6")
+
+	println(player)
+
 }
